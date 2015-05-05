3748c5ad1e9ea3ec678bb736e0e114f703e0d7a0
Brik Allen Royster <broyster@node46.boisestate.edu>
2015-05-04 19:58:55 -0600

    initial bitmap test code using libbmp

--- src/bitmap_test.c
+++ src/bitmap_test.c
@@ -1,35 +1,19 @@
-/*
- * Code for testing generation of bitmaps
- * Originally copied from http://www.noktec.be/archives/360
- */
+#include <bmpfile.h>
 
-#include <stdio.h>
- 
-int main(void){
- 
-FILE *f;
-int filesize = 54 + 3*1*1;
- 
-unsigned char bmpfileheader[14] = {'B','M', 0,0,0,0, 0,0,0,0, 54,0,0,0};
-unsigned char bmpinfoheader[40] = {40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0, 24,0};
-unsigned char bmpcontent[6] = {0,0,255};
-unsigned char bmppad[3] = {0,0,0};
- 
-/* Construct header with filesize part */
-        bmpfileheader[ 2] = (unsigned char)(filesize    );
-        bmpfileheader[ 3] = (unsigned char)(filesize>> 8);
-        bmpfileheader[ 4] = (unsigned char)(filesize>>16);
-        bmpfileheader[ 5] = (unsigned char)(filesize>>24);
- 
-        /* Construct header with width and height part */
-        bmpinfoheader[ 4] = (unsigned char)(1);
-        bmpinfoheader[ 8] = (unsigned char)(1);
- 
-        f = fopen("test.bmp","wb");
-        fwrite(bmpfileheader,1,14,f);
-        fwrite(bmpinfoheader,1,40,f);
-        fwrite(bmpcontent,3,6,f);
- 
-        fclose(f);
-return 1;
+int main() {
+    bmpfile_t *bmp;
+    rgb_pixel_t color = {0xFF, 0, 0, 0}; // blue green red alpha
+    
+    bmp = bmp_create(200, 200, 24); //width, height, depth
+    int x, y;
+    for(x = 0; x < 200; x++) { //write color in all pixels
+        for(y = 0; y < 200; y++) {
+            bmp_set_pixel(bmp, x, y, color);
+        }
+    }
+
+    bmp_save(bmp, "test.bmp");
+    bmp_destroy(bmp);
+
+    return 0;
 }
